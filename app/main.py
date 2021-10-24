dbd073367ae68589c4271b12683c812d394c1f01
luisiul359 <luisiul359@gmail.com>
2021-10-24 22:28:38 +0100

    Undo FastAPI dummy example

--- app/main.py
+++ app/main.py
@@ -14,16 +14,12 @@ app = FastAPI()
 
 #data_initiatives = extract_data()
 #if Path("data_initiatives_votes.pkl").is_file():
-#data_initiatives_votes = pd.read_pickle("data_initiatives_votes.pkl")
+data_initiatives_votes = pd.read_pickle("data_initiatives_votes.pkl")
 #else:
 #data_initiatives_votes = get_initiatives_votes(data_initiatives)
 #pd.to_pickle(data_initiatives_votes, "data_initiatives_votes.pkl")
 
 
-#@app.get("/party-approvals")
-#def get_party_approvals(type: Optional[str] = None, dt_ini: Optional[str] = None, dt_fin: Optional[str] = None):
-#    return json.loads(votes.get_party_approvals(data_initiatives_votes).to_json(orient="index"))
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/party-approvals")
+def get_party_approvals(type: Optional[str] = None, dt_ini: Optional[str] = None, dt_fin: Optional[str] = None):
+    return json.loads(votes.get_party_approvals(data_initiatives_votes).to_json(orient="index"))
