1bfbcf90708b7241399f3588c75608d98c7efce6
luisiul359 <luisiul359@gmail.com>
2021-11-20 19:45:47 +0000

    Fix worker

--- worker.py
+++ worker.py
@@ -1,7 +1,7 @@
 import os
 
 import redis
-from rq import Worker, Queue
+from rq import Worker, Queue, Connection
 
 
 redis_url = os.getenv("REDIS_URL")
@@ -10,8 +10,10 @@ redis_url = os.getenv("REDIS_URL")
 if __name__ == '__main__':
     # connect to Redis
     conn = redis.from_url(redis_url)
-    q = Queue(connection=conn)
+
+    listen = ['high', 'default', 'low']
 
     # create the worker and process the request
-    worker = Worker(q)
-    worker.work()
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
