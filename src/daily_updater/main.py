a47e533b5467b787f21d867e3bba5147cc8e09d7
luisiul359 <luisiul359@gmail.com>
2022-07-09 15:23:20 +0100

    Update scheduler

--- src/daily_updater/main.py
+++ src/daily_updater/main.py
@@ -131,7 +131,7 @@ def get_blob_container() -> BlobContainerClient:
     return container_client
 
 
-@sched.scheduled_job("cron", hour="3", minute="00")
+@sched.scheduled_job("cron", hour="14", minute="30")
 def main() -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
