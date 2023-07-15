8840f0e796ff3fa75fd7c9993b897c43f6c621cc
FARFETCH\luis.baia <luis.baia@farfetch.com>
2023-07-15 14:48:17 +0100

    auto format project

--- src/app/apis/schemas.py
+++ src/app/apis/schemas.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import List, Dict
+
 from pydantic import BaseModel
 
 
--- src/daily_updater/parliament/votes.py
+++ src/daily_updater/parliament/votes.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import numpy as np
-
-from typing import List
 from collections import defaultdict
+from typing import List
+
+import numpy as np
+import pandas as pd
 
 
 def get_party_approvals(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
@@ -12,9 +12,9 @@ def get_party_approvals(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
 
     if len(data_initiatives_votes) == 0:
         return pd.DataFrame()
-    
+
     data_initiatives_votes = data_initiatives_votes.copy()
-    
+
     # get all party votes fields
     parties_vote_direction_fields = [x for x in data_initiatives_votes.columns if x.startswith("iniciativa_votacao")]
     to_exclude = "iniciativa_votacao_res iniciativa_votacao_desc iniciativa_votacao_outros_afavor iniciativa_votacao_outros_abstenção iniciativa_votacao_outros_contra iniciativa_votacao_unanime".split()
@@ -23,7 +23,8 @@ def get_party_approvals(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
     # when the vote was unanimous, individual party vote direction is empty
     # with this code we fix it
     unanime_rows = data_initiatives_votes["iniciativa_votacao_unanime"] == "unanime"
-    new_values = data_initiatives_votes.loc[unanime_rows, parties_vote_direction_fields].applymap(lambda x: "afavor" if pd.isna(x) else x)  # move all NaN values to "afavor"
+    new_values = data_initiatives_votes.loc[unanime_rows, parties_vote_direction_fields].applymap(
+        lambda x: "afavor" if pd.isna(x) else x)  # move all NaN values to "afavor"
     data_initiatives_votes.loc[unanime_rows, parties_vote_direction_fields] = new_values
 
     def calculate_vote_distribution(group: pd.DataFrame, parties_vote_direction_fields: List[str]) -> pd.Series:
@@ -40,7 +41,9 @@ def get_party_approvals(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
 
         return res
 
-    return data_initiatives_votes.groupby("iniciativa_autor").apply(lambda x: calculate_vote_distribution(x, parties_vote_direction_fields)).sort_values("total_iniciativas", ascending=False)
+    return data_initiatives_votes.groupby("iniciativa_autor").apply(
+        lambda x: calculate_vote_distribution(x, parties_vote_direction_fields)).sort_values("total_iniciativas",
+                                                                                             ascending=False)
 
 
 def get_party_correlations(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
@@ -68,13 +71,13 @@ def get_party_correlations(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame
             else:
                 corr = pd.crosstab(pa[indexes], pb[indexes], margins=True)
                 diag = np.diag(corr)
-                
+
                 total = diag[-1]
                 total_corr = diag[:-1].sum()
 
                 res[party_a].append(total_corr / total)
 
-    return pd.DataFrame(res, index=parties_columns).reset_index().rename(columns = {'index':'nome'})
+    return pd.DataFrame(res, index=parties_columns).reset_index().rename(columns={'index': 'nome'})
 
 
 def collect_parties_strange_votes(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
@@ -101,16 +104,17 @@ def collect_parties_strange_votes(data_initiatives_votes: pd.DataFrame) -> pd.Da
 
     data = []
     for col in parties_columns:
-        party = col.replace("iniciativa_votacao_", "").replace("cr", "CRISTINA RODRIGUES").replace("jkm", "JOACINE KATAR MOREIRA").upper()
+        party = col.replace("iniciativa_votacao_", "").replace("cr", "CRISTINA RODRIGUES").replace("jkm",
+                                                                                                   "JOACINE KATAR MOREIRA").upper()
 
-        mask = (data_initiatives_votes["iniciativa_autor"] == party) & ((data_initiatives_votes[col] != "afavor") | data_initiatives_votes[col].isnull())
+        mask = (data_initiatives_votes["iniciativa_autor"] == party) & (
+                    (data_initiatives_votes[col] != "afavor") | data_initiatives_votes[col].isnull())
         errors = data_initiatives_votes.loc[mask, col]
 
         data.append({
             "party": party,
             "invalid_entries": len(errors) / (data_initiatives_votes["iniciativa_autor"] == party).sum()
         })
-
 
     return pd.DataFrame(data).set_index("party")
 
@@ -121,7 +125,7 @@ def get_initiatives(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
     """
 
     data_initiatives_votes = data_initiatives_votes.copy()
-    
+
     # get needed fields' name
     parties_vote_direction_fields = [x for x in data_initiatives_votes.columns if x.startswith("iniciativa_votacao")]
     to_exclude = "iniciativa_votacao_res iniciativa_votacao_desc iniciativa_votacao_outros_afavor iniciativa_votacao_outros_abstenção iniciativa_votacao_outros_contra iniciativa_votacao_unanime".split()
@@ -130,19 +134,24 @@ def get_initiatives(data_initiatives_votes: pd.DataFrame) -> pd.DataFrame:
     # when the vote was unanimous, individual party vote direction is empty
     # with this code we fix it
     unanime_rows = data_initiatives_votes["iniciativa_votacao_unanime"] == "unanime"
-    new_values = data_initiatives_votes.loc[unanime_rows, parties_vote_direction_fields].applymap(lambda x: "afavor" if pd.isna(x) else x)  # move all NaN values to "afavor"
+    new_values = data_initiatives_votes.loc[unanime_rows, parties_vote_direction_fields].applymap(
+        lambda x: "afavor" if pd.isna(x) else x)  # move all NaN values to "afavor"
     data_initiatives_votes.loc[unanime_rows, parties_vote_direction_fields] = new_values
 
     # add addtional column
-    data_initiatives_votes["iniciativa_url_res"] = "https://www.parlamento.pt/ActividadeParlamentar/Paginas/DetalheIniciativa.aspx?BID=" + data_initiatives_votes["iniciativa_id"]
+    data_initiatives_votes[
+        "iniciativa_url_res"] = "https://www.parlamento.pt/ActividadeParlamentar/Paginas/DetalheIniciativa.aspx?BID=" + \
+                                data_initiatives_votes["iniciativa_id"]
 
-    columns = ["iniciativa_evento_fase", "iniciativa_titulo", "iniciativa_url", "iniciativa_url_res", "iniciativa_autor", "iniciativa_autor_deputados_nomes", "iniciativa_evento_data", "iniciativa_tipo", "iniciativa_votacao_res"] + parties_vote_direction_fields
+    columns = ["iniciativa_evento_fase", "iniciativa_titulo", "iniciativa_url", "iniciativa_url_res",
+               "iniciativa_autor", "iniciativa_autor_deputados_nomes", "iniciativa_evento_data", "iniciativa_tipo",
+               "iniciativa_votacao_res"] + parties_vote_direction_fields
 
     df = data_initiatives_votes[columns].rename({
         "iniciativa_evento_data": "iniciativa_data",
         "iniciativa_evento_fase": "iniciativa_fase"
-        }, axis="columns")
+    }, axis="columns")
 
     df["iniciativa_data"] = df["iniciativa_data"].dt.strftime("%Y-%m-%d")
-    
+
     return df.reset_index().rename(columns={"index": "iniciativa_id"})
