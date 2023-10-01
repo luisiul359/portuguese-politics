8aefb2b0ee2ea127294108e643d9e91b8cb5ee42
jopijuco <jpjc@protonmail.com>
2023-10-01 12:12:06 +0100

    Initiative endpoint

---
+++ src/app/parliament/routers/initiatives.py
@@ -0,0 +1,25 @@
+import time
+
+from fastapi import APIRouter
+
+from src.app.parliament.config.constants import CURRENT_LEGISLATURE
+from src.app.parliament.models.initiatives import Iniciativa, ProjectoPropostaLei
+from src.app.parliament.models.parliament import Legislature
+from src.app.parliament.services.initiatives import get_initiative as service_get_initiative
+
+
+router = APIRouter(
+    prefix="/initiatives",
+    tags=["Initiative"],
+    # dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+# For future initiative types, they should inherit from Iniciativa
+# and also be added as Union[type1, type2, ...] on the response model
+@router.get("/{id}", response_model=ProjectoPropostaLei)
+def get_initiative(id: int, legislature: Legislature = Legislature.XV) -> Iniciativa:
+    #timer = time.time()
+    initiative = service_get_initiative(int(id), legislature)
+    #print(f"Timer {time.time() - timer:0.3}")
+    return initiative
