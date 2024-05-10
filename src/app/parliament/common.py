1a71a24b0d9896737bac1a9ebb07ab71e90dd9d2
Joao Coelho <jpjc@protonmail.com>
2024-05-10 21:10:19 +0100

    First commit

---
+++ src/app/parliament/common.py
@@ -0,0 +1,5 @@
+from parliament.models.legislature import Legislatura
+
+
+supported_legislatures = [Legislatura.XIV, Legislatura.XV, Legislatura.XVI]
+current_legislature = list(supported_legislatures)[-1]
