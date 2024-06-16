47b1e0a1113a96ec78c9daf6085f181ac160b99c
Joao Coelho <jpjc@protonmail.com>
2024-06-16 23:12:08 +0100

    parlamentar deputies endpoint

---
+++ src/app/parliament/common.py
@@ -0,0 +1,5 @@
+from app.parliament.model.legislature import Legislatura
+
+
+supported_legislatures = [Legislatura.XIV, Legislatura.XV, Legislatura.XVI]
+current_legislature = list(supported_legislatures)[-1]
